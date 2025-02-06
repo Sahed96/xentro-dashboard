d5b5d4e56eddad7f48261815a93eccdf7f46243b
Sahed96 <ninja.sahedkhan@gmail.com>
2025-02-06 13:41:43 +0600

    intialize dashboard and make sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,48 @@
+import { MdAddToPhotos } from 'react-icons/md';
+import logo from '/xentro.jpg';
+import { FaTachometerAlt, FaUsers, FaBox } from 'react-icons/fa';
+
+const Sidebar = () => {
+  return (
+    <div className="bg-gray-100 text-gray-900 h-screen px-4 fixed w-16 md:w-64 border-r border-gray-300 dark:border-gray-600 dark:bg-gray-900 dark:text-white">
+      <div className="flex gap-2 mt-4 justify-center items-center">
+        <img className="w-8 h-8 rounded-full" src={logo} alt="" />
+        <p className="text-2xl font-bold hidden md:block text-center italic">
+          Xentro
+        </p>
+      </div>
+      <ul className="flex flex-col mt-5 text-xl">
+        <li
+          className="flex items-center py-3 px-2 space-x-4 hover:rounded hover:cursor-pointer 
+        hover:bg-blue-600 hover:text-white"
+        >
+          <FaTachometerAlt />
+          <span className="hidden md:inline">Dashboard</span>
+        </li>
+        <li
+          className="flex items-center py-3 px-2 space-x-4 hover:rounded hover:cursor-pointer 
+        hover:text-white hover:bg-blue-600"
+        >
+          <FaUsers />
+          <span className="hidden md:inline ">Users</span>
+        </li>
+        <li
+          className="flex items-center py-3 px-2 space-x-4 hover:rounded hover:cursor-pointer 
+        hover:text-white hover:bg-blue-600"
+        >
+          <MdAddToPhotos />
+          <span className="hidden md:inline ">Add Products</span>
+        </li>
+        <li
+          className="flex items-center py-3 px-2 space-x-4 hover:rounded hover:cursor-pointer 
+        hover:text-white hover:bg-blue-600"
+        >
+          <FaBox />
+          <span className="hidden md:inline ">Products</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
